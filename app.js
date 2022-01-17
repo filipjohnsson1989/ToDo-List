0c753fa3a30f7c447779816bf1bcc4b6b83bafcb
Filip Johnsson <Filip.johnsson1989@gmail.com>
2022-01-17 20:57:30 +0100

    tasks 1 to 5

--- app.js
+++ app.js
@@ -1,18 +1,49 @@
 "use strict";
+import Item from "./item.js";
 
-(function (w) {
+(function (todo) {
+    const items = document.querySelector('#toDoItems');
 
-    
-
-
-
-    function run() {
+    todo.addItem = function(e){
         
+        e.preventDefault();
+        const input = this['todoInput'];
+        const li = createToDo(input.value);
+        items.appendChild(li);
+        input.value = '';
+    }
+
+    const createToDo = (input) => {
+        let li = document.createElement('li');
+        li.innerText = input;
+        li.classList.add('list-group-item');
+        li.addEventListener('click', change);
+
+        let button = document.createElement('button');
+        button.classList.add('btn', 'btn-danger','float-end');
+        button.innerText = 'X';
+        button.addEventListener('click', remove);
+
+        li.appendChild(button);
+
+        return li;
+    }
+
+    function change(){
+        this.classList.toggle('bg-success');
+        this.classList.toggle('text-decoration-line-through');
+        this.classList.toggle('bg-opacity-25');
 
     }
 
-
-    run();
+    function remove(){
+        this.parentElement.remove();
+    }
 
 })(window.todo = window.todo || {});
+
+
+document.querySelector('#todoForm').addEventListener('submit', todo.addItem);
+
+
 
